1e61dc2de1c84bac2544f6a68708b918f2dd9525
molten <gabpj1209@gmail.com>
2025-11-20 20:51:23 -0600

    The actual program itself
    not much to say but its very basic and maybe non efficient

---
+++ fileorganizer.py
@@ -0,0 +1,101 @@
+import os
+import shutil
+import time
+
+file_exts = [
+    ".png", # index 0
+    ".jpg", # index 1
+    ".webp", # index 2
+    ".exe", # index 3
+    ".mp4", # index 4
+    ".webm", # index 5
+    ".mp3", # index 6
+    ".wav", # index 7
+    ".pdf", # index 8
+    ".epub", # index 9
+    ".mobi", # index 10
+    ".zip", # index 11
+]
+folder_names = [
+    "Images", # index 0
+    "Videos", # index 1
+    "Apps", # index 2
+    "Files", # index 3
+    "Audio", # index 4
+    "Documents", # index 5
+]
+cwd = os.chdir('C:/Users/user/Downloads')
+files = os.listdir(cwd)
+
+def foldersconfirm():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    folder_confirmation = input("Create Folders [y/n]:")
+    if folder_confirmation == "y" or folder_confirmation == 'Y':
+        createfolders()
+    elif folder_confirmation == "n" or folder_confirmation == 'N':
+        currentpath()
+        getfiles()
+        movefiles()
+    else:
+        foldersconfirm()
+
+def currentpath():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print()
+    print("-" * 30)
+    print("Current Directory: " + os.getcwd())
+    print("-" * 30)
+    print()
+
+def getfiles():
+    #get list of files
+    for i in range(0, len(files)):
+        print(files[i])
+        i += 1
+    print()
+
+def createfolders():
+    for i in range(0, len(folder_names)):
+        try:
+            os.mkdir(folder_names[i])
+            i += 1
+            if i == len(folder_names):
+                print("Folders Created.")
+        except:
+            print("Folder " + folder_names[i] + " already exists.")
+            i += 1
+    time.sleep(2)
+    currentpath()
+    getfiles()
+    movefiles()
+
+
+def movefiles():
+    moveconfirm = input("Move files to folders?: [y/n]:")
+    if moveconfirm == "y" or moveconfirm == 'Y':
+        for i in range(0, len(files)):
+            filename, filetype = os.path.splitext(files[i])
+            if filetype in file_exts[:3]:
+                shutil.move(files[i], "./Images")
+            elif filetype == file_exts[3]:
+                shutil.move(files[i], "./Apps")
+            elif filetype in file_exts[4:6]:
+                shutil.move(files[i], "./Videos")
+            elif filetype in file_exts[6:8]:
+                shutil.move(files[i], "./Audio")
+            elif filetype in file_exts[8:11]:
+                shutil.move(files[i], "./Documents")
+            elif filetype == file_exts[11]:
+                shutil.move(files[i],"./Files")
+            else:
+                pass
+        print("Actions Done.")
+        time.sleep(2)
+    elif moveconfirm == "n" or moveconfirm == 'N': 
+        print("No Actions Done.")
+        time.sleep(2)
+    else:
+        movefiles()
+
+
+foldersconfirm()
